62b2a53cd01bba92a2e0a1c4bfa4092d254347f1
pikapikapow <vladimirhwang84@gmail.com>
2024-05-19 11:13:24 -0400

    Learned some basics of python

--- python.py
+++ python.py
@@ -1,1 +1,25 @@
+from math import *
+
 print("Hello World")
+
+# variables and data types
+
+character_name = "Mr. John"
+character_age = "35"
+
+print(character_name + " is " + character_age)
+print(f"{character_name} is {character_age}")
+
+print(floor(3.90))
+
+print(abs(-7))
+
+print(sqrt(25))
+
+# \n = new line
+# \" = puts " in without breaking string
+# len(variable_name) = variable_name.length()
+# .replace("old str", "new str") = replaces strings
+# abs(num) = absolute value
+# pow(num, exponent) = exponents
+# from ___ import * = imports everything from a library, such as math
