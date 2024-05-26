0fa9834604e7e2f902f9b0f05b94a79f885c709d
pikapikapow <vladimirhwang84@gmail.com>
2024-05-26 09:48:30 -0400

    Learned about functions and lists

--- python.py
+++ python.py
@@ -1,39 +1,66 @@
-from math import *
+# Notes
 
-print("Hello World")
+# Python is a zero based language! [0] = the first item.
 
-# variables and data types
+# Variable  notes
 
-character_name = "Mr. John"
-character_age = "35"
 
-print(character_name + " is " + character_age)
-print(f"{character_name} is {character_age}")
-
-# Functions
-
-print(floor(3.90))
-
-print(abs(-7))
-
-print(sqrt(25))
-
-# Inputs
-
-# name = input("Enter your name:")
-#
-# print(f"Hello {name}!")
-
-#calc
-
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-result = float(num1) + float(num2)
-print(result)
 # \n = new line
 # \" = puts " in without breaking string
 # len(variable_name) = variable_name.length()
 # .replace("old str", "new str") = replaces strings
+
+# Number notes
+
 # abs(num) = absolute value
 # pow(num, exponent) = exponents
 # from ___ import * = imports everything from a library, such as math
+
+# List notes
+
+# num = [2, 3, 4] num[-1] = 4. num[-2] = 3. Negative index starts from the last item going backward.
+# [1:] = all list items starting from the second one. [1:4] All items from the second to, but not including, the fifth.
+# .extend() adds a list to the end of your current list. Ex. num.extend(people) = [2, 3, 4, "Derek", "Sam", "Jake"]
+# .append() adds one value to the end of your current list. Ex num.append(5) = [2, 3, 4, 5]
+# .insert() takes 2 values.The index you want your new value to be in, and the new value. Ex num.insert(2,9) = [2,3,9,4]
+# .remove() removes a value that you put in. Not index of the value, the value itself!
+# .clear() clears a list.
+# .pop() pops the last value out of a list.
+# .index() finds the index of a value. Value isn't in list? Error!
+# .count() counts how much values are inside the list.
+# .sort() sorts values in an array by alphabetical order if they're words, and ascending order if numbers. Both = error
+# .copy() creates a list copy
+# Tuples are like lists, but cannot change!
+
+# Function notes
+
+# Function code is indented. Stop indenting and you're not in that function anymore.
+# Parameters are same as JS
+# Return is same as JS
+
+# If statements
+
+# Ifs are very similar in Python to JS. Instead of {}, it's : Indented code is also in use for ifs. else is not indented
+
+# Lists
+
+people = ["Derek", "Sam", "Jake"]
+
+people.sort()
+print(people)
+
+# Functions
+
+def sayhi():
+    print("Hello")
+
+sayhi()
+
+# If statements
+
+is_yummy = False
+
+if is_yummy:
+    print("Yum yum!")
+else:
+    print("Ewwwww!")
